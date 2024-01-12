9e3e308b4963c2fd164e07d49b86f641107762f9
James MacAdie (TT sandbox) <j.macadie@ttg.co.uk>
2024-01-12 11:38:42 +0000

    Fix compile script & Ctrl F6 shortcut

--- compile.sh
+++ compile.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Left side
+rm ferris_left.uf2
 sed -i 's?// #define INIT_EE_HANDS_LEFT?#define INIT_EE_HANDS_LEFT?' keyboards/ferris/keymaps/jmacadie/config.h
 sed -i 's?#define INIT_EE_HANDS_RIGHT?// #define INIT_EE_HANDS_RIGHT?' keyboards/ferris/keymaps/jmacadie/config.h
 qmk compile
-mv ferris_sweep_jmacadie_rp2040_ce.uf2 ferris_left.uf2
+mv ferris_sweep_jmacadie.uf2 ferris_left.uf2
 
 # Right side
+rm ferris_right.uf2
 sed -i 's?#define INIT_EE_HANDS_LEFT?// #define INIT_EE_HANDS_LEFT?' keyboards/ferris/keymaps/jmacadie/config.h
 sed -i 's?// #define INIT_EE_HANDS_RIGHT?#define INIT_EE_HANDS_RIGHT?' keyboards/ferris/keymaps/jmacadie/config.h
 qmk compile
-mv ferris_sweep_jmacadie_rp2040_ce.uf2 ferris_right.uf2
+mv ferris_sweep_jmacadie.uf2 ferris_right.uf2
